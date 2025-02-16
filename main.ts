966f16a4fe9802b8257999d5c832403e9248bd3d
elluchoco813 <luis.coral@utp.edu.co>
2025-02-16 17:24:18 -0500

    voltaje crudo

--- main.ts
+++ main.ts
@@ -141,7 +141,7 @@ namespace groveina226 {
         readShuntVoltage(): number {
             let rawShuntVoltage = this.readRegister(INA226_SHUNT_VOLTAGE); // Leer el registro de voltaje del shunt
             let voltageShunt = rawShuntVoltage * 0.0000025; // 2.5 µV por bit (específico del INA226)
-            return Math.round(voltageShunt * 10000) / 10000; // Redondear a 4 decimales
+            return Math.round(voltageShunt); // Redondear a 4 decimales
         }
 
         //% blockID=grove_ina_readvoltage block="%ina|Medir Voltaje"
